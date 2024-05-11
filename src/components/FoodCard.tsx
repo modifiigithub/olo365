69e5ec7eb38762377ee1a755e86b1777267a9e1f
Md. Mehedi Hasan Nabil <mehedihasannabil49@gmail.com>
2024-05-11 09:56:57 +0600

    hero section design

--- src/components/FoodCard.tsx
+++ src/components/FoodCard.tsx
@@ -22,8 +22,8 @@ export default function FoodCard({ food }: { food: FoodItem }) {
             <div className="card-body">
                 <h2 className="card-title font-bold text-xl">{food.title}</h2>
                 <p className="line-clamp-2 text-stone-600 text-base mb-2">{food.description}</p>
-                <div className="flex">
-                    <p className="text-lg font-bold">$12.99</p>
+                <div className="flex items-center">
+                    <p className="text-xl font-bold">$12.99</p>
                     <div className="card-actions justify-end">
                         <button className="btn btn-sm px-6 py-1 text-white rounded-full bg-brand-600 hover:bg-brand-500">
                             <FaCartPlus />
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,18 @@
+import heroImage from "../assets/images/hero-bg.jpg"
 
 export default function Hero() {
     return (
-        <div className="h-[60vh] flex justify-center items-center bg-slate-100 mt-4">
-            <h3 className="text-3xl">Slider</h3>
+        <div className="relative bg-cover bg-center flex justify-center items-center"
+            style={{ backgroundImage: `url(${heroImage})` }}
+        >
+            <div className="absolute inset-0 bg-gradient-to-r from-stone-900/50 to-stone-900/40"></div>
+
+            <div className="text-center py-36 text-white z-10">
+                <h3 className="text-6xl font-semibold">Indulge in Culinary Delights and </h3>
+                <h3 className="text-5xl font-semibold mt-8">Unforgettable Dining Experiences</h3>
+
+                <button className="btn px-6 text-base border-brand-800 bg-brand-600 hover:bg-brand-500 text-white mt-12">Book a Table</button>
+            </div>
         </div>
     )
 }
